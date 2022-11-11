e94d6783ff54897ca856e42d9bebb885f136c02e
Hemant Sharma <Hemant Sharma@LAPTOP-3F2FGE57>
2022-11-11 23:48:55 +0530

    Add project files.

---
+++ Interfaces/IProductInventoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public interface IProductInventoryRepository
+    {
+        Dictionary<string, int> GetInventory();
+        void UpdateInventory(string code);
+    }
+}
